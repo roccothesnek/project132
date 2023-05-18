145d30a47bf334c364e7c623b70da51a639fef1b
Jadyn Falls <101415931+JadynF@users.noreply.github.com>
2023-05-17 22:38:09 -0500

    Delete gittest.py

--- gittest.py
+++
@@ -1,9 +0,0 @@
-## git commands ##
-# git add: add file to staging area
-# git commit: commit changes to the repository
-# remember to add a comment
-# git push: push changes to remote repository
-# git pull: pull changes from remote repository
-# git status: show the status of the local repository
-# git log: show history of changes  
-# git diff: show changes between commits
